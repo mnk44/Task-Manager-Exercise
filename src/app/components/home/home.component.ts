9ef1092f40fb4bb775e221c4fd764f5882fe5ea1
mnk44 <mnkmm44@gmail.com>
2023-12-28 18:51:34 -0500

    [Build] creating the initial component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: []
+})
+export class HomeComponent {
+
+}
