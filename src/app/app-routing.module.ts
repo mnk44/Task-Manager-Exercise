0dc8f8372caa23893bf3ee3f8fd5a46303202f56
mnk44 <mnkmm44@gmail.com>
2023-12-31 03:30:03 -0500

    [Feature] creating sign up component and connecting with the home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home', pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./components/signup/signup.component').then(m => m.SignUpComponent)
   }
 ];
 
---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sign-up',
+  standalone: true,
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+  imports: []
+})
+export class SignUpComponent {
+
+}
