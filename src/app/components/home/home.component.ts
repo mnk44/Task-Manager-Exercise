369a7bb8b4a4103746b0d08ade7e7d16c08f3b36
mnk44 <mnkmm44@gmail.com>
2024-01-02 16:20:25 -0500

    [Feature] validate input at login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: []
+  imports: [ FormsModule ]
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  email: string = '';
+  password: string = '';
 }
