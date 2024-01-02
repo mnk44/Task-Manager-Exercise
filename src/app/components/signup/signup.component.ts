c850ecd7adf1b0bbd25a4699601045ea4d296382
mnk44 <mnkmm44@gmail.com>
2024-01-02 16:18:52 -0500

    [Build] finishing sign up deployment

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { LocalStorage } from 'src/app/services/localstorage.service';
 
 @Component({
